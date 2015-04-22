384ba8577c6e08fa909a731d7837da85c32cb4ae
BrandonYates <brandonyates66@gmail.com>
2015-04-21 23:51:31 -0500

    game loop added

---
+++ app/src/main/java/com/syndrisoftware/beyourownboss/MainThread.java
@@ -0,0 +1,81 @@
+package com.syndrisoftware.beyourownboss;
+
+/**
+ * Created by brandon on 4/21/15.
+ */
+
+import android.graphics.Canvas;
+import android.view.SurfaceHolder;
+
+public class MainThread extends Thread
+{
+    private int FPS = 30;                 //maximum fps cap
+    private double averageFPS;            //stores average fps
+    private SurfaceHolder surfaceHolder;
+    private GamePanel gamePanel;
+    private boolean running;
+    public static Canvas canvas;
+
+    public MainThread(SurfaceHolder surfaceHolder, GamePanel gamePanel)
+    {
+        super();
+        this.surfaceHolder = surfaceHolder;
+        this.gamePanel = gamePanel;
+    }
+    @Override
+    public void run()
+    {
+        long startTime;
+        long timeMillis;
+        long waitTime;
+        long totalTime = 0;
+        int frameCount = 0;
+        long targetTime = 1000/FPS;
+
+        while(running) {
+            startTime = System.nanoTime();
+            canvas = null;
+
+            //try locking the canvas for pixel editing
+            try {
+                canvas = this.surfaceHolder.lockCanvas();
+                synchronized (surfaceHolder) {
+                    this.gamePanel.update();
+                    this.gamePanel.draw(canvas);
+                }
+            } catch (Exception e) {
+            }
+            finally{
+                if(canvas!=null)
+                {
+                    try {
+                        surfaceHolder.unlockCanvasAndPost(canvas);
+                    }
+                    catch(Exception e){e.printStackTrace();}
+                }
+            }
+
+            timeMillis = (System.nanoTime() - startTime) / 1000000;
+            waitTime = targetTime-timeMillis;                       //determine waitTime needed
+                                                                   //to have exactly 30 frames each minute
+
+            try{
+                this.sleep(waitTime);                             //actually wait
+            }catch(Exception e){}
+
+            totalTime += System.nanoTime()-startTime;
+            frameCount++;
+            if(frameCount == FPS)
+            {
+                averageFPS = 1000/((totalTime/frameCount)/1000000); //calculate  average fps
+                frameCount =0;
+                totalTime = 0;
+                System.out.println(averageFPS);
+            }
+        }
+    }
+    public void setRunning(boolean b)
+    {
+        running=b;
+    }
+}
